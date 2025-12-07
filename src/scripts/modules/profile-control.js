6de87a9e33801c59528eb4a19702a045feb38ced
AkmalKurbanov <20296a@gmail.com>
2025-12-07 19:14:42 +0600

    fix profile

--- src/scripts/modules/profile-control.js
+++ src/scripts/modules/profile-control.js
@@ -1,14 +1,27 @@
 const avatar = document.querySelector('.avatar-js');
 const profileControl = document.querySelector('.profile-control');
+const profileBlock = document.querySelector('.profile-control__block');
 const closeBtn = document.querySelector('.profile-control__close');
 
-
-avatar.addEventListener('click', () => {
+avatar.addEventListener('click', (e) => {
+  e.stopPropagation();
   profileControl.classList.add('is-open');
 });
 
-
 closeBtn.addEventListener('click', (e) => {
-  e.stopPropagation(); // чтобы клик по кнопке не срабатывал как клик по .avatar-js
+  e.stopPropagation();
+  profileControl.classList.remove('is-open');
+});
+
+document.addEventListener('click', (e) => {
+  const t = e.target;
+
+  // Клик по аватару — НЕ закрываем
+  if (avatar.contains(t)) return;
+
+  // Клик внутри блока — НЕ закрываем
+  if (profileBlock.contains(t)) return;
+
+  // Все остальные клики — закрываем
   profileControl.classList.remove('is-open');
 });
